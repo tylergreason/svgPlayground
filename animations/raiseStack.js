1d948ea2419f321f731f33ae699b957fec377558
tylergreason <tyler.greason@protonmail.com>
2020-07-03 09:58:28 -0400

    Made functions for moving a stack of elements forwards and backwards smoothly

--- animations/raiseStack.js
+++ animations/raiseStack.js
@@ -1,16 +1,43 @@
 let wkas = Array.from(document.getElementsByClassName('wka')); 
-wkas.forEach(ele => console.log(ele))
 
 // function to return raising frames 
 let framesUp = (ele,i) => {
     // find element's height  
-    let eleHeight = eleStyle(ele).height; 
-    let eleTop = eleStyle(ele).top; 
-    console.log(eleHeight + eleTop);
+    let eleHeight = parseStyleValue(eleStyle(ele).height); 
+    let eleTop = parseStyleValue(eleStyle(ele).top); 
     
     // move element to a certain position - (height * i)
-    // return [
-    //     {top:eleTop},
-    //     {top:`${0 + }`},
-    // ]
+    return [
+        {top:eleTop+'px', easing: 'ease'},
+        // {top:`${0 + (eleHeight * i)}px`, easing: 'ease'},
+        {top:`${0 + (eleHeight * i)}px`},
+    ]
+}
+
+// function to return lowering frames 
+let framesDown = (ele,i) => {
+    // find element's height  
+    let eleHeight = parseStyleValue(eleStyle(ele).height); 
+    let eleTop = parseStyleValue(eleStyle(ele).top); 
+    
+    // move element to a certain position - (height * i)
+    return [
+        {top:eleTop+'px', easing: 'ease'},
+        // {top:`${0 + (eleHeight * i)}px`, easing: 'ease'},
+        {top:`100vh`},
+    ]
+}
+
+// function to move all wkas up 
+const raiseEles = array => {
+    array.forEach((ele, index) => {
+        ele.animate(framesUp(ele, index), {duration: 500, fill: 'forwards'})
+    })
+}
+
+// function to move all wkas down
+const lowerEles = array => {
+    array.forEach((ele, index) => {
+        ele.animate(framesDown(ele, index), {duration: 500, fill: 'forwards'})
+    })
 }
