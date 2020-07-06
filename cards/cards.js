360803e1d9c4306c3a9329d8e39c7d60f10819d4
tylergreason <tyler.greason@protonmail.com>
2020-07-06 10:42:06 -0400

    Added a React-esque component argument system utilizing objects to hold named arguments

--- cards/cards.js
+++ cards/cards.js
@@ -19,9 +19,10 @@ const createUnorderedList = array =>{
 
 let sampleArray = ['Aliqua esse ea ipsum et adipisicing nisi cupidatat.','Exercitation reprehenderit duis proident quis velit duis culpa laboris dolore cillum quis.','Tempor minim nulla aute aliqua occaecat tempor quis labore nostrud irure anim ut.']; 
 
-const createCard = (array) => {
+const createCard = (array, options={}) => {
+    // debugger
     return document.createRange().createContextualFragment(`
-        <div class='card__container'>
+        <div class='card__container ${options.classes}' data-id=${options.dataset}>
             <div class='card--left'>
             </div>
             <div class='card--info__container card--right'>
@@ -33,5 +34,5 @@ const createCard = (array) => {
 }
 
 // test above function 
-let test = createCard(sampleArray) 
+let test = createCard(sampleArray, {classes:'', dataset:78979}) 
 document.body.appendChild(test); 
